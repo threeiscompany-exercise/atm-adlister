079f46363286c2496f8298fed675aab467703910
Art <art_jr2000@yahoo.com>
2017-10-26 09:15:41 -0500

    creating search options

--- src/main/java/com/codeup/adlister/dao/Ads.java
+++ src/main/java/com/codeup/adlister/dao/Ads.java
@@ -9,5 +9,7 @@ public interface Ads {
     List<Ad> all();
     // insert a new ad and return the new ad's id
     Long insert(Ad ad);
+
+    List<Ad> search(String searchAd);
 //    Ad viewOneAd(Long id);
 }
